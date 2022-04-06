269ee1b7c737d19802ebbadcb2bfe286feba3941
Cristian Perdomo <ferperdomo14@gmail.com>
2022-04-06 17:40:49 +0200

    hero

--- portfolio/src/Components/Navigation/Navigation.jsx
+++ portfolio/src/Components/Navigation/Navigation.jsx
@@ -1,8 +1,9 @@
-import { Navbar, Container, Nav, NavLink } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 import './Navigation.css'
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light">
             <Container>
                 <Navbar.Brand href="/">Ferbperdomo</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -10,10 +11,10 @@ const Navigation = () => {
                     <NavLink to='/whoami'>
                         <Nav.Link as="span">Who am I?</Nav.Link>
                     </NavLink>
-                    <NavLink to="/">
+                    <NavLink to="/projects">
                         <Nav.Link as="span">My projects</Nav.Link>
                     </NavLink>
-                    <NavLink to="/">
+                    <NavLink to="/contact">
                         <Nav.Link as="span">Contact me :)</Nav.Link>
                     </NavLink>
                 </Navbar.Collapse>
--- portfolio/src/pages/IndexPage/IndexPage.jsx
+++ portfolio/src/pages/IndexPage/IndexPage.jsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 import '../IndexPage/IndexPage.css'
 
 
@@ -6,10 +6,18 @@ const IndexPage = () => {
 
 
     return (
-        <Container>
-            <h1>This is my profile :)</h1>
-            {/* <video src="./../../../../head.MOV" muted autoplay loop></video> */}
-        </Container>
+        <section id='hero'>
+            <Container >
+                <article className='introduction'>
+                    <p>Hi! <img className='wavy-hand' src="https://media.tenor.com/images/30169e4a670daf12443df7d2dd140176/tenor.gif"></img> I am</p>
+                    <p className='brand'>Cristian Perdomo.</p>
+                    <p>FullStack Developer</p>
+                    <Button href="https://www.linkedin.com/in/cristian-perdomo-parra/" variant="outline-light">Linkedin</Button>
+                    <Button href='https://github.com/ferbperdomo' variant="outline-light">Github</Button>
+                </article>
+
+            </Container>
+        </section>
     )
 }
 
